fda4f6a383a530851f846e16a31ce28879e833e7
JemboDev <sapryginalex@gmail.com>
2021-03-16 19:47:29 +0600

    basic requirements for project

---
+++ find_flight_deals.py
@@ -0,0 +1,14 @@
+'''
+This script uses Sheety API and Tequila API
+to search for cheap flights to user's chosen destinations
+'''
+# TODO: get "prices" sheet info (think about lowest prices)
+# TODO: get IATA codes for chosen cities
+# TODO: get cheapest flight prices
+    # 1: From ALA to destination city
+    # 2: Direct flights (if no, then 1 stop, 2, 3 etc.)
+    # 3: Between tomorrow and in 6 months time
+    # 4: Round trips only
+    # 5: Return time between 7 and 14 days
+    # 6: Currency in KZT
+# TODO: set some kind of notification if prices are really low
